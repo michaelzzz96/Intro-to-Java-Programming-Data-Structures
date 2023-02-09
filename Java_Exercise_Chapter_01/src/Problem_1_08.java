6cad87b025c2f0ed7b26dd18edcc82cfbb429a77
Mike Z <37784194+michaelzzz96@users.noreply.github.com>
2023-02-08 20:54:20 -0500

    Add files via upload

---
+++ Java_Exercise_Chapter_01/src/Problem_1_08.java
@@ -0,0 +1,8 @@
+public class Problem_1_8 {
+
+    public static void main(String[] args) {
+        System.out.println("Perimeter = 2 * radius * PI with radius 5.5 is " + 2 * 5.5 * 3.14);
+        System.out.println("Area = radius * radius * PI with radius 5.5 is " + 5.5 * 5.5 * 3.14);
+    }
+
+}
---
+++ Java_Exercise_Chapter_01/src/Problem_1_09.java
@@ -0,0 +1,8 @@
+public class Problem_1_9 {
+
+    public static void main(String[] args) {
+        System.out.println("Area = Width * Height with width = 4.5 and height = 7.9. Area is " + 4.5 * 7.9);
+
+    }
+
+}
