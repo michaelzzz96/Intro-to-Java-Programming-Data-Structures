ecebce09e9f75837d621107197a69e853e6d0b3b
Mike Z <37784194+michaelzzz96@users.noreply.github.com>
2023-05-11 21:41:50 -0400

    Add files via upload

---
+++ Java_Exercise_Chapter_4/src/Problem_4_16.java
@@ -0,0 +1,12 @@
+
+public class Problem_4_16 {
+
+    public static void main(String[] args) {
+
+      double letter =  (65 + Math.random() * 26);
+      System.out.print((char)letter);
+
+
+    }
+
+}
