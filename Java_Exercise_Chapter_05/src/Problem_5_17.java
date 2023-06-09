76c3dc9c470e50ff5d83bca27b81bd14e186590b
Mike Z <37784194+michaelzzz96@users.noreply.github.com>
2023-06-08 21:54:03 -0400

    Add files via upload

---
+++ Java_Exercise_Chapter_05/src/Problem_5_17.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Problem_5_17 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        //get the total number of lines n.
+        System.out.print("Enter the number of lines: ");
+        int n = input.nextInt();
+
+        //Loop through the lines from 1 to n
+        for (int i = 1; i <= n; i++) {
+            // printing spaces, 2 at a time from j=1 to j= n-i
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print("  ");
+            }
+
+            //Printing number decreamentally from the line number j to 1
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+
+            //Printing number increamentally from 2 to line number j
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+
+        }
+
+    }
+
+}
+
+
