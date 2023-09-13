e928ebd3c67946cdb6ce99dda78bc857b5d3592d
Mike Z <37784194+michaelzzz96@users.noreply.github.com>
2023-09-13 16:38:26 -0400

    Add files via upload

---
+++ Java_Exercise_Chapter_07/src/Problem_7_21.java
@@ -0,0 +1,14 @@
+public class Problem_7_21 {
+
+    public static void main (String [] args){
+        int sum=0;
+
+        for(int i=0; i< args.length; i++){
+            sum+= Integer.parseInt(args[i]);
+        }
+
+        System.out.println("Sum of numbers: " +sum);
+    }
+
+
+}
