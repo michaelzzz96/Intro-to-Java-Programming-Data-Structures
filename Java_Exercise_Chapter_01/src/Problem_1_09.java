47243cd2e601a2ded748a80c06ce6b201025ec64
mike <michaelzzz1996@gmail.com>
2023-09-29 19:09:48 -0400

    Commit

--- Java_Exercise_Chapter_01/src/Problem_1_09.java
+++ Java_Exercise_Chapter_01/src/Problem_1_09.java
@@ -1,4 +1,4 @@
-public class Problem_1_9 {
+public class Problem_1_09 {
 
     public static void main(String[] args) {
         System.out.println("Area = Width * Height with width = 4.5 and height = 7.9. Area is " + 4.5 * 7.9);
