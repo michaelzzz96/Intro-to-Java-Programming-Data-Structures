4a699f100909ce484a6e4f4534b122c62e241321
Mike Z <37784194+michaelzzz96@users.noreply.github.com>
2024-01-27 20:37:08 -0500

    Add files via upload

---
+++ Java_Exercise_Chapter_12/src/Problem_12_19.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Problem_12_19 {
+
+    public static void main(String[] args) {
+        try {
+            // Create a URL object
+            java.net.URL url = new java.net.URL(
+                    "http://cs.armstrong.edu/liang/data/Lincoln.txt");
+
+            int count = 0; // Counts words
+
+            // Open an input stream and create a Scanner object
+            Scanner input = new Scanner(url.openStream());
+            while (input.hasNext()) {
+                // Read words
+                if (Character.isLetter((input.next()).charAt(0))) {
+                    count++;
+                }
+            }
+
+            // Display result
+            System.out.println(
+                    "Number of words in President Abraham Lincoln's Gettysburg address: " +
+                            count);
+        }
+        catch (java.net.MalformedURLException ex) {
+            System.out.println("Invalid URL");
+        }
+        catch (java.io.IOException ex) {
+            System.out.println("I/0 Errors: no such file");
+        }
+    }
+
+}
